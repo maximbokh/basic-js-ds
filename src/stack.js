b27e5f4b3ef698bedb38b3cf355d909e78568dc8
Maksim Bokhanka <isupermaximus@gmail.com>
2023-04-16 23:58:43 +0300

    feat: add solution for queue, stack tasks

--- src/stack.js
+++ src/stack.js
@@ -13,19 +13,18 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 class Stack {
-  push(/* element */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  mas = [];
+
+  push(element) {
+    this.mas.push(element);
   }
 
   pop() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.mas.pop();
   }
 
   peek() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.mas[this.mas.length-1];
   }
 }
 
